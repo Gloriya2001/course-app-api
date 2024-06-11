dd1b39291bb6605678f2db703ce1642a043d7679
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 21:44:27 +0530

    api of add is done

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const {coursemodel} = require("./model/course")
 
 const app = express()
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 app.post("/add",(req,res)=>{
+
+    //res.send("success")
+
+    let input = req.body
+    //console.log(input)
+    //res.send("success")
+
+    let course = new coursemodel(input)
+    //res.send("success")
+    course.save()//save to db
+    console.log(course)
     res.send("success")
+    //to get this output it is must that express.json() in the above
 })
 
 app.get("/view",(req,res)=>{
