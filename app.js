9daef2c6fb29c3b8953f6d1e213d18ef3a47f6da
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 22:19:01 +0530

    api of view is done and cloud db link adde

--- app.js
+++ app.js
@@ -7,24 +7,37 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+mongoose.connect("mongodb+srv://gloria2001:gloria2001@cluster0.ipg35w1.mongodb.net/courseDB?retryWrites=true&w=majority&appName=Cluster0")
+
 app.post("/add",(req,res)=>{
 
     //res.send("success")
 
-    let input = req.body
+    let input = req.body//1.input read
     //console.log(input)
     //res.send("success")
 
-    let course = new coursemodel(input)
+    let course = new coursemodel(input)//2.pass input to model 
     //res.send("success")
-    course.save()//save to db
+    course.save()//3.save to db
     console.log(course)
-    res.send("success")
+     res.json({"status":"success"})//res.send("success")
     //to get this output it is must that express.json() in the above
 })
 
 app.get("/view",(req,res)=>{
-    res.send("view")
+
+    //res.send("view")
+//use find to retrive value form db. if find is true executed in the thena()
+    coursemodel.find().then(
+
+        (data) => {
+            res.json(data)
+        }
+
+    )
+
 })
 
 
